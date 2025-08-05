7a9ede07beef41b79716db6b82f124be942c9d8f
trungkien1006 <cristntk0162@gmail.com>
2025-08-06 02:12:00 +0700

    feat: testing husky

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+export function App() {
+  return <div className="">
+      <div className="mx-auto">
+        {/* <header className="mb-6">
+          <h1 className="text-3xl font-bold text-blue-800">Hub-wise</h1>
+          <p className="text-gray-600">
+            Your source for cross-field information
+          </p>
+        </header> */}
+        <main className="h-screen">
+        </main>
+        {/* <footer className="mt-10 text-center text-gray-500 text-sm">
+          <p>© {new Date().getFullYear()} Hub-wise. All rights reserved.</p>
+        </footer> */}
+      </div>
+    </div>;
+}
